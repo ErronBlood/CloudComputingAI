8cc75972266f5b30f4b8bbdcfa4cf64c5d53fc3d
ErronBlood <gabrielgomaestre@gmail.com>
2025-11-06 20:48:42 -0500

    Implementacion de regresion logistica

---
+++ tests/logisticRegressionParams.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import kagglehub
+from kagglehub import KaggleDatasetAdapter
+
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from imblearn.over_sampling import SMOTE
+
+df = kagglehub.dataset_load(
+  KaggleDatasetAdapter.PANDAS,
+  "sandhyapeesara/cloud-anomaly-data",
+  "Cloud_Anomaly_Dataset.csv"
+)
+
+df1 = df.dropna(subset=['cpu_usage', 'memory_usage', 'network_traffic', 'power_consumption',
+                   'num_executed_instructions', 'execution_time', 'energy_efficiency'])
+df1 = df1.drop(columns=['vm_id', 'timestamp', 'task_type', 'task_priority', 'task_status'])
+
+# Split features (X) and target (y)
+X = df1.drop(columns=['Anomaly status'])
+y = df1['Anomaly status']
+
+# Apply SMOTE
+smote = SMOTE(random_state=42)
+X_resampled, y_resampled = smote.fit_resample(X, y)
+
+# Recreate the balanced dataframe
+dfResampled = pd.DataFrame(X_resampled, columns=X.columns)
+dfResampled['Anomaly status'] = y_resampled
+
+x_train, x_test, y_train, y_test = train_test_split(X_resampled, y_resampled, test_size=0.20)
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(x_train)
+X_test_scaled = scaler.transform(x_test)
+
+param_grid = {
+    'C': [0.00001, 0.0001, 0.001],  # Regularization strength
+    'penalty': ['l1', 'l2', 'elasticnet'],  # Regularization type
+    'solver': ['liblinear', 'saga'],  # Optimization algorithm
+    'class_weight': [None],  # Handle any remaining imbalance
+    'max_iter': [1000, 1500,2000]  # Maximum iterations
+}
+
+lr = LogisticRegression()
+comb = GridSearchCV(lr, param_grid, cv = 5, scoring = 'recall')
+
+comb.fit(X_train_scaled,y_train)
+
+print("Mejor combinacion de parametros:", comb.best_params_)
+print("Puntaje de mejores parametros: ", comb.best_score_)
+
+bestComb = comb.best_estimator_
+
+y_pred = bestComb.predict(X_test_scaled)
+
+print('Accuracy de LogisticRegression sobre el conjunto de prueba es: {:.2f}'.format(bestComb.score(X_test_scaled, y_test))) 
+cmatrix = confusion_matrix(y_test, y_pred)
+print(cmatrix)
+
+print(classification_report(y_test, y_pred))
+print(X_resampled.columns)
